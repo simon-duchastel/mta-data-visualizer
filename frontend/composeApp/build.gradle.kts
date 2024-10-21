400313f87d4c1a16b38d839a777f6ea611005198
Simon Duchastel <simon@duchastel.com>
2024-10-21 03:04:43 -0400

    WIP - Android app

--- frontend/composeApp/build.gradle.kts
+++ frontend/composeApp/build.gradle.kts
@@ -1,9 +1,11 @@
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsCompose)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.kotlin.serialization)
@@ -29,9 +31,21 @@ kotlin {
         }
         binaries.executable()
     }
+
+    androidTarget {
+        @OptIn(ExperimentalKotlinGradlePluginApi::class)
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
     
     sourceSets {
-        
+
+        androidMain.dependencies {
+            implementation(compose.preview)
+            implementation(libs.androidx.activity.compose)
+        }
+
         commonMain.dependencies {
             // Compose
             implementation(compose.runtime)
@@ -58,4 +72,33 @@ kotlin {
     }
 }
 
+android {
+    namespace = "com.duchastel.simon.mtadatavisualizer"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
 
+    defaultConfig {
+        applicationId = "com.duchastel.simon.mtadatavisualizer"
+        minSdk = libs.versions.android.minSdk.get().toInt()
+        targetSdk = libs.versions.android.targetSdk.get().toInt()
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    debugImplementation(compose.uiTooling)
+}
