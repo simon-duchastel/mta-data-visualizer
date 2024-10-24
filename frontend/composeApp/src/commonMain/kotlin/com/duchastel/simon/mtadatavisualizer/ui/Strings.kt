c8dfa476a827a08edf70699e84f515ad0c28bc6f
simon-duchastel <41980219+simon-duchastel@users.noreply.github.com>
2024-10-23 23:54:05 -0700

    Add dark mode, prettify (#11)
    * Merge
    
    * Format ridership, add strings, add dark mode
    
    * Format ridership, add strings, add dark mode
    
    * Fix icon, fix dark/light mode button
    
    * swap order of light/dark toggle

---
+++ frontend/composeApp/src/commonMain/kotlin/com/duchastel/simon/mtadatavisualizer/ui/Strings.kt
@@ -0,0 +1,34 @@
+package com.duchastel.simon.mtadatavisualizer.ui
+
+import io.ktor.util.date.WeekDay
+import mta_data_visualizer.composeapp.generated.resources.Res
+import mta_data_visualizer.composeapp.generated.resources.friday_short
+import mta_data_visualizer.composeapp.generated.resources.monday_short
+import mta_data_visualizer.composeapp.generated.resources.saturday_short
+import mta_data_visualizer.composeapp.generated.resources.sunday_short
+import mta_data_visualizer.composeapp.generated.resources.thursday_short
+import mta_data_visualizer.composeapp.generated.resources.tuesday_short
+import mta_data_visualizer.composeapp.generated.resources.wednesday_short
+import org.jetbrains.compose.resources.StringResource
+
+fun formatAsString(weekDay: WeekDay): StringResource {
+    return when (weekDay) {
+        WeekDay.MONDAY -> Res.string.monday_short
+        WeekDay.TUESDAY -> Res.string.tuesday_short
+        WeekDay.WEDNESDAY -> Res.string.wednesday_short
+        WeekDay.THURSDAY -> Res.string.thursday_short
+        WeekDay.FRIDAY -> Res.string.friday_short
+        WeekDay.SATURDAY -> Res.string.saturday_short
+        WeekDay.SUNDAY -> Res.string.sunday_short
+    }.also {
+        println(it)
+    }
+}
+
+fun formatRidershipString(ridership: Int): String {
+    return ridership.toString()
+        .reversed()
+        .chunked(3)
+        .joinToString(",")
+        .reversed()
+}
--- frontend/composeApp/src/commonMain/kotlin/com/duchastel/simon/mtadatavisualizer/ui/ridershipticker/RidershipTickerViewModel.kt
+++ frontend/composeApp/src/commonMain/kotlin/com/duchastel/simon/mtadatavisualizer/ui/ridershipticker/RidershipTickerViewModel.kt
@@ -7,6 +7,7 @@ import io.ktor.util.date.WeekDay
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.getAndUpdate
 import kotlinx.coroutines.launch
 
 class RidershipTickerViewModel(
@@ -31,26 +32,7 @@ class RidershipTickerViewModel(
 
     init {
         fetchRidership()
-
-        viewModelScope.launch {
-            _state.collect { state ->
-                if (state.ridership != null && state.ridershipPerSecond != null) {
-                    delay(STATE_UPDATE_DELAY_MS)
-
-                    // normalize to the frequency we're updating the state
-                    val factor: Float = 1_000f / STATE_UPDATE_DELAY_MS
-                    val newRidership = state.ridership.numRiders +
-                            (state.ridershipPerSecond / factor)
-                    updateState {
-                        copy(
-                            ridership = state.ridership.copy(
-                                numRiders = newRidership
-                            )
-                        )
-                    }
-                }
-            }
-        }
+        updateRidership()
     }
 
     // Public functions
@@ -73,7 +55,7 @@ class RidershipTickerViewModel(
                 val ridership = subwayDataService.getTodaysRidership()
 
                 val ridershipPerSecond = ridership?.let {
-                    it.ridersPerHour / 60f / 60f // riders/s = riders/hr * 60min * 60s
+                    it.ridersPerHour / 60f / 60f // riders/s = riders/hr / 60min / 60s
                 }
                 updateState {
                     copy(
@@ -90,8 +72,29 @@ class RidershipTickerViewModel(
         }
     }
 
+    // Update ridership based on per second projection, or no-op if ridership
+    // is uninitialized.
+    private fun updateRidership() {
+        viewModelScope.launch {
+            while (true) {
+                delay(STATE_UPDATE_DELAY_MS)
+
+                updateState {
+                    if (ridership != null && ridershipPerSecond != null) {
+                        // normalize to the frequency we're updating the state
+                        val factor: Float = 1_000f / STATE_UPDATE_DELAY_MS
+                        val newRidership = ridership.numRiders + (ridershipPerSecond / factor)
+                        copy(ridership = ridership.copy(numRiders = newRidership))
+                    } else {
+                        this
+                    }
+                }
+            }
+        }
+    }
+
     private fun updateState(newState: State.() -> State) {
-        _state.value = _state.value.newState()
+        _state.getAndUpdate(newState)
     }
 
     companion object {
